fd881b4e70fc67cefd08e9829019bd4e3bef23cf
datnguyen2001 <nguyendatdz01@gmail.com>
2024-10-08 13:57:41 +0700

    first commit

--- resources/views/web/become_pilot/index.blade.php
+++ resources/views/web/become_pilot/index.blade.php
@@ -18,24 +18,24 @@
                         {!! @$data->content !!}
                     </div>
                     <div class="pc9 ">
-                        @if($data->title_one)
+                        @if(isset($data->title_one))
                             <h2 class="pc10 ">{{@$data->title_one}}</h2>
                         @endif
                         <div class="pc11 ">
                             <div class="pc12 row">
                                 <div class="pc20 col-md-5">
                                     <div class="pc21 ">
-                                        @if($data->src_1)
+                                        @if(isset($data->src_1))
                                             <p class="pc22 "><img class="pc23 " src="{{asset(@$data->src_1)}}"></p>
                                         @endif
                                     </div>
                                 </div>
                                 <div class="pc13 col-md-7">
                                     <div class="pc14 ">
-                                        @if($data->name_1)
+                                        @if(isset($data->name_1))
                                             <h3 class="pc15 ">{{@$data->name_1}}</h3>
                                         @endif
-                                        @if($data->content_1)
+                                        @if(isset($data->content_1))
                                             <div class="">{!! @$data->content_1 !!}
                                             </div>
                                         @endif
@@ -47,24 +47,24 @@
                             <div class="pc12 row">
                                 <div class="pc20 col-md-5 visible-xs  visible-sm">
                                     <div class="pc21 ">
-                                        @if($data->src_2)
+                                        @if(isset($data->src_2))
                                             <p class="pc22 "><img class="pc23 " src="{{asset(@$data->src_2)}}"></p>
                                         @endif
                                     </div>
                                 </div>
                                 <div class="pc13 col-md-7">
                                     <div class="pc14 ">
-                                        @if($data->name_2)
+                                        @if(isset($data->name_2))
                                             <h3 class="pc15 ">{{@$data->name_2}}</h3>
                                         @endif
-                                        @if($data->name_2)
+                                        @if(isset($data->content_2))
                                             <div class="pc16x">{!! @$data->content_2 !!}</div>
                                         @endif
                                     </div>
                                 </div>
                                 <div class="pc20 col-md-5 hidden-xs hidden-sm">
                                     <div class="pc21 ">
-                                        @if($data->src_2)
+                                        @if(isset($data->src_2))
                                             <p class="pc22 "><img class="pc23 " src="{{asset(@$data->src_2)}}"></p>
                                         @endif
                                     </div>
@@ -75,17 +75,17 @@
                             <div class="pc12 row">
                                 <div class="pc20 col-md-5">
                                     <div class="pc21 ">
-                                        @if($data->src_3)
+                                        @if(isset($data->src_3))
                                             <p class="pc22 "><img class="pc23 " src="{{asset(@$data->src_3)}}"></p>
                                         @endif
                                     </div>
                                 </div>
                                 <div class="pc13 col-md-7">
                                     <div class="pc14 ">
-                                        @if($data->name_3)
+                                        @if(isset($data->name_3))
                                             <h3 class="pc15 ">{{@$data->name_3}}</h3>
                                         @endif
-                                        @if($data->content_3)
+                                        @if(isset($data->content_3))
                                             <div class="pc16x">{!! @$data->content_3 !!}</div>
                                         @endif
                                     </div>
@@ -96,24 +96,24 @@
                             <div class="pc12 row">
                                 <div class="pc20 col-md-5 visible-xs  visible-sm">
                                     <div class="pc21 ">
-                                        @if($data->src_4)
+                                        @if(isset($data->src_4))
                                             <p class="pc22 "><img class="pc23 " src="{{asset(@$data->src_4)}}"></p>
                                         @endif
                                     </div>
                                 </div>
                                 <div class="pc13 col-md-7">
                                     <div class="pc14 ">
-                                        @if($data->name_4)
+                                        @if(isset($data->name_4))
                                             <h3 class="pc15 ">{{@$data->name_4}}</h3>
                                         @endif
-                                        @if($data->content_4)
+                                        @if(isset($data->content_4))
                                             <div class="pc16x">{!! @$data->content_4 !!}</div>
                                         @endif
                                     </div>
                                 </div>
                                 <div class="pc20 col-md-5 hidden-xs hidden-sm">
                                     <div class="pc21 ">
-                                        @if($data->src_4)
+                                        @if(isset($data->src_4))
                                             <p class="pc22 "><img class="pc23 " src="{{asset(@$data->src_4)}}"></p>
                                         @endif
                                     </div>
@@ -124,17 +124,17 @@
                             <div class="pc12 row">
                                 <div class="pc20 col-md-5">
                                     <div class="pc21 ">
-                                        @if($data->src_5)
+                                        @if(isset($data->src_5))
                                             <p class="pc22 "><img class="pc23 " src="{{asset(@$data->src_5)}}"></p>
                                         @endif
                                     </div>
                                 </div>
                                 <div class="pc13 col-md-7">
                                     <div class="pc14 ">
-                                        @if($data->name_5)
+                                        @if(isset($data->name_5))
                                             <h3 class="pc15 ">{{@$data->name_5}}</h3>
                                         @endif
-                                        @if($data->content_5)
+                                        @if(isset($data->content_5))
                                             <div class="pc16x">{!! @$data->content_5 !!}</div>
                                         @endif
                                     </div>
@@ -145,44 +145,44 @@
                             <div class="pc12 row">
                                 <div class="pc20 col-md-5 visible-xs  visible-sm">
                                     <div class="pc21 ">
-                                        @if($data->src_6)
+                                        @if(isset($data->src_6))
                                             <p class="pc22 "><img class="pc23 " src="{{asset(@$data->src_6)}}"></p>
                                         @endif
                                     </div>
                                 </div>
                                 <div class="pc13 col-md-7">
                                     <div class="pc14 ">
-                                        @if($data->name_6)
+                                        @if(isset($data->name_6))
                                             <h3 class="pc15 ">{{@$data->name_6}}</h3>
                                         @endif
-                                        @if($data->content_6)
+                                        @if(isset($data->content_6))
                                             <div class="pc16x">{!! @$data->content_6 !!}</div>
                                         @endif
                                     </div>
                                 </div>
                                 <div class="pc20 col-md-5 hidden-xs hidden-sm">
                                     <div class="pc21 ">
-                                        @if($data->src_6)
+                                        @if(isset($data->src_6))
                                             <p class="pc22 "><img class="pc23 " src="{{asset(@$data->src_6)}}"></p>
                                         @endif
                                     </div>
                                 </div>
                             </div>
                         </div>
-                        @if($data->title_two)
+                        @if(isset($data->title_two))
                             <h2 class="pc10 ">{{@$data->title_two}}</h2>
                         @endif
-                        @if($data->content_two)
+                        @if(isset($data->content_two))
                             <div class="pc11 pc11-6">
                                 <div class="pc12 chi-tiet-bai-viet">
                                     {!! @$data->content_two !!}
                                 </div>
                             </div>
                         @endif
-                        @if($data->title_three)
+                        @if(isset($data->title_three))
                             <h2 class="pc10 ">{{@$data->title_three}}</h2>
                         @endif
-                        @if($data->content_three)
+                        @if(isset($data->content_three))
                             <div class="pc11 pc11-5">
                                 <div class="pc12 ">
                                     <div class="pc13 ">
@@ -193,10 +193,10 @@
                                 </div>
                             </div>
                         @endif
-                        @if($data->title_four)
+                        @if(isset($data->title_four))
                             <h2 class="pc10 pc10x text-center">{{@$data->title_four}}</h2>
                         @endif
-                        @if($data->content_four)
+                        @if(isset($data->content_four))
                             <div class="pc11 pc11-5">
                                 <div class="pc12 ">
                                     <div class="pc13 ">
